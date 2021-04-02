84049ec9f2dd1b02e7c033f35660d3070cb04ee2
Zakariae <zhamedoun1@gmail.com>
2021-04-02 22:53:15 +0200

    Add files via upload

---
+++ test_ultrasonic_sensor.ino
@@ -0,0 +1,26 @@
+// Pins
+const int trig = A2;
+const int echo = A1;
+
+void setup(){
+  pinMode(trig, OUTPUT);
+  digitalWrite(echo, LOW);
+  Serial.begin(9600);
+}
+
+void loop(){
+  distance();
+  delay(2000);
+}
+
+void distance(){
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  int duree = pulseIn(echo,HIGH);
+  float dist = 0.034*duree/2;
+  Serial.print("La distance est :");
+  Serial.print(dist);
+  Serial.print("\n");
+  //return 0;
+}
